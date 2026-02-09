250b593526eb8b6103c125c85b962a871f2df05c
teleg <telegash@gmail.com>
2026-02-09 12:16:38 +0300

    lab1 completedddd

---
+++ src/ru/hse/lab1/Main.java
@@ -0,0 +1,18 @@
+package ru.hse.lab1;
+
+import ru.hse.lab1.second.Person;
+/**
+ * @author Батраков Артём Олегович
+ * @version 1.0
+ * @since 2026
+ * 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person p = new Person("Иван", 20);
+        p.printInfo();
+
+    }
+}
